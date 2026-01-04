cd7edbc3e713a4a4bae62164d1a762db6f13a673
hyeonsik971029 <156991031+hyeonsik971029@users.noreply.github.com>
2026-01-04 22:51:51 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.swift
+++ 프로그래머스/2/42842. 카펫/카펫.swift
@@ -1,10 +1,11 @@
 import Foundation
 
 // 카펫은 항상 직사각형 혹은 정사각형 모양을 유지한다..
+// Int로 계산했을 때, 우연히 맞는 경우가 있음... 조심할 것...
 func solution(_ brown:Int, _ yellow:Int) -> [Int] {
 
     var yellow_c: Double = 1.0
-    var yellow_r: Double = Double(yellow) / yellow_c
+    var yellow_r: Double = Double(yellow)
     
     while yellow_r >= yellow_c {
         if yellow_r * 2 + yellow_c * 2 + 4.0 == Double(brown) {
